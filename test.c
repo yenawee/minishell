e1bf93f4003929949edff0a8adef5c2c923a1eba
yenawee <91937954+yenawee@users.noreply.github.com>
2022-06-21 15:32:10 +0900

    Add files via upload

---
+++ test.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+//readline
+void readline_test(){
+
+	char *line_read;
+	const char *prompt = "$";
+	int a;
+
+	line_read = readline(prompt);
+	while (line_read)
+	{
+		if (line_read && *line_read)
+		{
+			add_history(line_read);
+		}
+		line_read = readline(prompt);
+	}
+}
+
+
+//signal
+void handle_sigtstp(int sig)
+{
+    printf("stop not allowed\n");
+
+}
+
+void _signal()
+{
+
+	signal(SIGTSTP, &handle_sigtstp);
+
+    int x;
+    printf("input number : ");
+    scanf("%d", &x);
+    printf("input number : %d, result : %d\n", x, x * 5);
+}
+
+//kill
+void	_kill()
+{
+    int pid = fork();
+    if (pid == -1) return ;
+
+    if (pid == 0)
+    {
+        while (1)
+        {
+            printf("some output\n");
+            usleep(50000);
+        }
+    }
+    else
+    {
+        kill(pid, SIGSTOP);
+        int t;
+        do {
+            printf("put time: ");
+            scanf("%d", &t);
+
+            if ( t > 0)
+            {
+                kill(pid, SIGCONT);
+                sleep(t);
+                kill(pid, SIGSTOP);
+            }
+        } while (t > 0);
+        kill(pid, SIGKILL);
+        wait(NULL);
+    }
+}
+
+//getcwd
+void	_getcwd()
+{
+	char *buf;
+
+	buf = NULL;
+	char *path = getcwd(buf, 0);
+	printf("%s\n", path);
+}
+
+//chdir
+void _chdir()
+{
+	int ret = chdir("../../");
+	printf("%d\n", ret);
+
+	system("pwd");
+}
+
+//opendir, readdir
+void	_dir()
+{
+    DIR *dp;
+    struct dirent *dirp;
+
+    dp = opendir("./");
+
+    while ((dirp = readdir(dp)) != NULL)
+        printf("%s\n", dirp->d_name);
+
+    closedir(dp);
+    exit(EXIT_SUCCESS);
+}
+
+
+int main()
+{
+	//readline_test();
+	//_signal();
+	//_kill();
+	//_getcwd();
+	//_chdir();
+	//_dir();
+
+	return 0;
+}
