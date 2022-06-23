99afb7e7939d249c11407f3501a99d55ccafafca
jhj9109 <jhj91_09@naver.com>
2022-06-23 19:43:30 +0900

    [Update] parse_quote.c

--- srcs/parse_quote.c
+++ srcs/parse_quote.c
@@ -1,123 +1,156 @@
-#include "../include/minishell.h"
+#include "minishell.h"
 
-void	test_list(t_str	*head)
+void	_temp_join(t_list *head, char *res)
 {
-	t_str	*cur;
+	t_list	*cur;
+	char	*s;
 
+	*res = 0;
 	cur = head;
 	while (cur)
 	{
-		printf("%s\n", cur->str);
+		s = (char *)cur->content;
+		while (*s)
+			*res++ = *s++;
 		cur = cur->next;
 	}
-	printf("res: |");
-	cur = head;
-	while (cur)
-	{
-		printf("%s", cur->str);
-		cur = cur->next;
-	}
-	printf("|\n");
+	*res = '\0';
 }
 
-char	*make_str(char *front, char *end)
+void	_temp_size(t_list *head, size_t	*res)
 {
-	char	*p;
+	t_list	*cur;
+
+	*res = 0;
+	cur = head;
+	while (cur)
+	{
+		*res = *res + ft_strlen((const char *)cur->content);
+		cur = cur->next;
+	}
+}
+
+void	_temp_print(t_list *head)
+{
+	t_list	*cur;
+
+	cur = head;
+	while (cur)
+	{
+		printf("==%s==\n", (char *)cur->content);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
+void	_parse_out_quote(char **p, t_list **head)
+{
+	char	*s;
+	char	*front;
 	char	*ret;
-	int		i;
+	t_list	*new_node;
 
-	ret = malloc(end - front + 2);
-	if (ret == NULL)
-		return (NULL);//에러 종료
-	p = front;
-	i = -1;
-	while (p + ++i <= end)
-		ret[i] = p[i];
-	ret[i] = '\0';
-	return (ret);
-}
-
-t_str	*new_list(char *s)
-{
-	t_str	*ret;
-	
-	ret = malloc(sizeof(t_str));
-	memset(ret, 0, sizeof(t_str));
-	if (ret == NULL)
-		return (NULL);//에러 슈팅
-	ret->str = s;
-	return (ret);
-}
-
-void	addstr_list(t_str **str, char *s)
-{
-	t_str	*cur;
-
-	printf("added => |%s|\n", s);
-	cur = *str;
-	if (cur == NULL)
-		*str = new_list(s);
-	else
+	s = *p;
+	front = *p;
+	while (*s != S_QUOTE && *s != D_QUOTE && *s != '\0')
+		s++;
+	if (front != s)
 	{
-		while (cur->next)
-			cur = cur->next;
-		cur->next = new_list(s);
+		ret = ft_substr(front, 0, (size_t)(s - front));
+		new_node = ft_lstnew((void *)ret);
+		if (ret == NULL || new_node == NULL)
+		{
+			ft_safe_free((void **)&ret);
+			exit(EXIT_FAILURE);
+		}
+		ft_lstadd_back(head, new_node);
+		*p = s;
+	}
+}
+
+void	_parse_single_quote(char **p, t_list **head)
+{
+	char	*s;
+	char	*front;
+	char	*ret;
+	t_list	*new_node;
+
+	s = *p;
+	front = *p;
+	if (*front == S_QUOTE)
+	{
+		s++;
+		while (*s != S_QUOTE)
+			s++;
+		ret = ft_substr(front, 1, (size_t)(s - front - 1));
+		new_node = ft_lstnew((void *)ret);
+		if (ret == NULL || new_node == NULL)
+		{
+			ft_safe_free((void **)&ret);
+			exit(EXIT_FAILURE);
+		}
+		ft_lstadd_back(head, new_node);
+		*p = s + 1;
+	}
+}
+
+void	_parse_double_quote(char **p, t_list **head)
+{
+	char	*s;
+	char	*front;
+	char	*ret;
+	t_list	*new_node;
+
+	s = *p;
+	front = *p;
+	if (*front == D_QUOTE)
+	{
+		s++;
+		while (*s != D_QUOTE)
+			s++;
+		ret = ft_substr(front, 1, (size_t)(s - front - 1));
+		new_node = ft_lstnew((void *)ret);
+		if (ret == NULL || new_node == NULL)
+		{
+			ft_safe_free((void **)&ret);
+			exit(EXIT_FAILURE);
+		}
+		ft_lstadd_back(head, new_node);
+		*p = s + 1;
 	}
 }
 
 void	parse_quote(char *s)
 {
-	// s: 12'3a'bc'45'6 => 123abc456
-	// s: 123\' 456 \" 123 \' 234 \" \" \' \" \'
-	// => 123 456 \" 123 \' 234 \" \" \' \" 
-
-	t_str	*parse_str;
+	t_list	*lst;
+	size_t	total_size;
 	char	*p;
-	
+
+	lst = NULL;
 	while (*s)
 	{
-		p = s;
-		while (*s != '\'' && *s != '\"' && *s != '\0')
-			s++;
-		if (p != s)
-		{
-			addstr_list(&parse_str, make_str(p, s - 1));
-			//1. 쿼츠밖 구간; p ~ s-1
-			if (*s == '\0')
-				break ;
-		}
-		p = s;
-		if (*s == '\'')
-		{
-			s++;
-			while (*s != '\'')
-				s++;
-		}
-		if (p != s)//2. 싱글쿼츠 안(싱글쿼츠 포함): p ~ s, (불포함): p+1 ~ s-1
-		{
-			addstr_list(&parse_str, make_str(p + 1, s - 1));
-			s++;
-		}
-		p = s;
-		if (*s == '\"')
-		{
-			s++;
-			while (*s != '\"')
-				s++;
-		}
-		if (*s == '\"')//3. 더블쿼츠 안(더블쿼츠 포함): p ~ s, (불포함): p+1 ~ s-1
-		{
-			addstr_list(&parse_str, make_str(p + 1, s - 1));
-			s++;
-		}
+		_parse_out_quote(&s, &lst);
+		_parse_single_quote(&s, &lst);
+		_parse_double_quote(&s, &lst);
 	}
-	test_list(parse_str);
+	// 링크드 리스트완성
+
+	// 링크드리스트 => join까지 대충 시행.
+	_temp_print(lst);
+	_temp_size(lst, &total_size);
+	printf("size => %zu\n", total_size);
+	char	*temp = malloc(total_size + 1);
+	if (temp)
+	{
+		_temp_join(lst, temp);
+		printf("joined => %s\n", temp);
+	}
 }
 
-int	main(void)
-{
-	// parse_quote("12'3a'bc'45'6");
-	// parse_quote("123\' 456 \" 123 \' 234 \" \" \' \" \'");
-	parse_quote("123\'abc 123 \" \' 456 \" \" \' \" \'");
-	//123 456 " 123 ' 234 " " ' " 
-}
+// int	main(void)
+// {
+// 	// parse_quote("12'3a'bc'45'6");
+// 	parse_quote("123\' 456 \" 123 \' 234 \" \" \' \" \'");
+// 	// parse_quote("123\'abc 123 \" \' 456 \" \" \' \" \'");
+// 	//123 456 " 123 ' 234 " " ' " 
+// }
