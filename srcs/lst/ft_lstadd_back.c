75faa980dceeb578ec8f694326ea66ceb0e09327
jhj9109 <jhj91_09@naver.com>
2022-06-23 19:43:14 +0900

    [Update] ft_lstadd_back

--- srcs/lst/ft_lstadd_back.c
+++ srcs/lst/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: hyeonjan <hyeonjan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 14:24:45 by hyeonjan          #+#    #+#             */
-/*   Updated: 2022/06/23 18:05:13 by hyeonjan         ###   ########.fr       */
+/*   Updated: 2022/06/23 19:42:54 by hyeonjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last_node;
 
-	if (!lst || !new)
-		return ;
 	last_node = ft_lstlast(*lst);
 	if (!last_node)
 		*lst = new;
