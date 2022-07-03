3843907e51c4afb8788ed7de064e7ae48ff8d7da
yenawee <yaena0319@naver.com>
2022-07-03 17:22:34 +0900

    [UPDATE] env_list -> env char array
    execve 에 넘길 char ** 로 변환

---
+++ parsetest/env_list_to_char_array.c
@@ -0,0 +1,66 @@
+#include "test.h"
+
+int	env_count(t_list	*env_list)
+{
+	t_list	*curr;
+	int		env_cnt;
+
+	curr = env_list;
+	env_cnt = 0;
+	while (curr)
+	{
+		env_cnt++;
+		curr = curr->next;
+	}
+	return (env_cnt);
+}
+
+char	*get_env_line(t_list *env_list)
+{
+	char	*ret;
+	char	*key;
+	char	*value;
+	t_env	*env;
+	char	*tmp;
+
+	env = (t_env *)env_list->content;
+	key = ft_strdup(env->key);
+	value = ft_strdup(env->value);
+	ret = ft_strjoin(key, "=");
+	tmp = ret;
+	ret = ft_strjoin(ret, value);
+	free(tmp);
+	return (ret);
+}
+
+char	**env_list_to_char_arr(t_list *env_list)
+{
+	char	**env_arr;
+	t_list	*curr;
+	int		env_cnt;
+	int		i;
+
+	curr = env_list;
+	env_cnt = env_count(env_list);
+	env_arr = ft_calloc(env_cnt + 1, sizeof(char *));
+	i = 0;
+	while (i < env_cnt)
+	{
+		env_arr[i] = get_env_line(curr);
+		curr = curr->next;
+		i++;
+	}
+	env_arr[i] = NULL;
+	return (env_arr);
+}
+
+// int main(int ac, char **av, char **envp)
+// {
+// 	t_list *env_list = envp_init(envp);
+
+// 	char **env_arr = env_list_to_char_arr(env_list);
+// 	for (int i = 0; env_arr[i]; i++)
+// 		printf("%s\n", env_arr[i]);
+// 	return 0;
+// }
+
