f0457a7a9e3ab8c6465a7523df698ff20c8791c8
yenawee <yaena0319@naver.com>
2022-07-04 22:08:05 +0900

    [UPDATE] builtin exit 구현

--- builtin/ft_exit.c
+++ builtin/ft_exit.c
@@ -1,6 +1,52 @@
-#include "../include/minishell.h"
+#include "../../include/minishell.h"
 
-int	ft_exit()
+/*
+	#include <stdlib.h>
+	noreturn void exit(int status);
+*/
+
+typedef struct s_token
 {
-	return (EXIT_SUCCESS);
+	char	*s;
+	int		type;
+}	t_token;
+
+int	g_status;
+
+int	ft_exit(t_list *args)
+{
+	const	ac = ft_lstsize(args);
+	int	status;
+
+	if (isatty(STDIN_FILENO))
+		printf("exit\n");
+	if (ac > 2)
+	{
+		printf("exit: too many arguments\n");
+		return (EXIT_FAILURE);
+	}
+	if (ac == 0)
+		status = g_status;
+	else if (!ft_atoi(((t_token *)args->content)->s, &status))
+	{
+		status = 255;
+		printf("exit: numeric argument required\n");
+	}
+	exit(status);
 }
+
+// int main(void)
+// {
+// 	pid_t	pid;
+// 	int		status;
+
+// 	status = -1;
+// 	pid = fork();
+// 	if (pid == 0)
+// 		exit(2 & 0xff);
+// 	else
+// 		waitpid(pid, &status, WUNTRACED);
+// 	printf("exit_status: %d\n", status);
+// 	if (WIFEXITED(status))
+// 		printf("exit_status2: %d\n", WEXITSTATUS(status));
+// }
