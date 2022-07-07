165176143b1d6bc209022083e7c06eacc79c2dfb
yenawee <yaena0319@naver.com>
2022-07-07 20:14:15 +0900

    [MODIFY] 헤더 파일 1차 정리

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yenawee <yenawee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 22:13:34 by hyeonjan          #+#    #+#             */
-/*   Updated: 2022/07/07 19:56:04 by yenawee          ###   ########.fr       */
+/*   Updated: 2022/07/07 20:13:40 by yenawee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
-#include "struct.h"
+# include "struct.h"
 
-void	*ft_calloc(size_t count, size_t size);
+//lst
 void	ft_lstadd_back(t_list	**head, t_list *new);
 void	ft_lstadd_front(t_list	**head, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
@@ -36,10 +36,14 @@ t_list	*ft_lstlast(t_list *lst);
 int		ft_lstsize(t_list *lst);
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_alert_lstnew(void *content);
 
+
+//utils
 char	*ft_strchr(const char *str, int c);
 char	*ft_strdup(const char *s1);
 void	*ft_calloc(size_t count, size_t size);
+void	*ft_alert_calloc(size_t count, size_t size);
 void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
@@ -47,17 +51,10 @@ int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	ft_safe_free(void **p);
-
-t_list	*envp_init(char **envp);
-int		ft_env(t_list *list);
-void	ft_del(void *p);
-void	ft_lst_remove_if(t_list **list, char *key);
-int		ft_unset(t_list **list, char **keys);
-void	ft_export_one(t_list **list, char *key, char *value, int plus_flag);
-int		ft_export(t_list **list, char **str);
-int		ft_export_no_arg(t_list *env_list);
-
-int		ft_pwd(void);
+void	ft_putstr_fd(int fd, char *msg);
+char	*ft_strtrim(char const *s1, char const *set);
+int		ft_isspace(char c);
+int		ft_isoperator(char c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -65,40 +62,57 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		is_valid_key_first(char c);
 int		is_valid_key_last(char c);
-
-// new
-void	safe_free(void **p);
-void	exit_msg(int exit_status, int fd, char *msg);
-void	ft_putstr_fd(int fd, char *msg);
-char	*ft_strtrim(char const *s1, char const *set);
-int		ft_isspace(char c);
-int		ft_isoperator(char c);
-char	*expand_str(char *str, t_list *env_list);
-t_list	*envp_init(char **envp);
-char	*find_value_in_env(char *key, t_list *env_list);
-char	*expand_env(char *str, t_list *env_list);
-void	exec_signals(void);
-t_token	*define_type(t_token *list);
-int		parse(t_token **list, char *line, t_list *env_list);
-void	*ft_alert_calloc(size_t count, size_t size);
-int		make_pipelines(t_pipeline **pipelines, t_token *tokens);
-char	*expand_in_heredoc(char *input, int fd, t_list *env_list);
-int		handle_heredoc(t_token *tokens, t_list *env_list);
-void	test_list(t_pipeline *list);
-int		ft_isalnum(int c);
-char	**env_list_to_char_arr(t_list *env_list);
-void	exec_signals(void);
-t_wildcard	*str_to_t_wildcard(char *str);
-int		is_matched(char *file_name, t_wildcard *wildcard);
-void	match_wildcard(char **file_names, int file_count, \
-						t_wildcard **wildcards, int wildcard_count);
-void	addtoken(t_token **list, char *begin, char *end, t_list *env_list);
-t_list	*ft_alert_lstnew(void *content);
 char	*ft_alert_strdup(const char *s1);
 char	*ft_alert_strjoin(char const *s1, char const *s2);
 void	ft_alert_str_append(char **prev, char *added);
 void	ft_safe_strjoin(char **ret, char *str);
+
+void	exit_msg(int exit_status, int fd, char *msg);
+
+//builtin
+int		ft_unset(t_list **list, char **keys);
+void	ft_export_one(t_list **list, char *key, char *value, int plus_flag);
+int		ft_export(t_list **list, char **str);
+int		ft_export_no_arg(t_list *env_list);
+int		ft_pwd(void);
+int		ft_env(t_list *list);
+void	ft_del(void *p);
+void	ft_lst_remove_if(t_list **list, char *key);
+
+
+//env
+t_list	*envp_init(char **envp);
+char	**env_list_to_char_arr(t_list *env_list);
+
+//signal
+void	exec_signals(void);
 void	set_input_signal(void);
 
+//wildcard
+t_wildcard	*str_to_t_wildcard(char *str);
+int			is_matched(char *file_name, t_wildcard *wildcard);
+void		match_wildcard(char **file_names, int file_count, \
+						t_wildcard **wildcards, int wildcard_count);
+
+//lexer
+int		parse(t_token **list, char *line, t_list *env_list);
+void	addtoken(t_token **list, char *begin, char *end, t_list *env_list);
+char	*expand_str(char *str, t_list *env_list);
+char	*expand_env(char *str, t_list *env_list);
+char	*find_value_in_env(char *key, t_list *env_list);
+t_token	*define_type(t_token *list);
+
+//parser
+int		make_pipelines(t_pipeline **pipelines, t_token *tokens);
+
+//heredoc
+char	*expand_in_heredoc(char *input, int fd, t_list *env_list);
+int		handle_heredoc(t_token *tokens, t_list *env_list);
+
+//execve
 void	make_cmd_argv(t_command *cmd, t_list *env_list);
+
+
+void	test_list(t_pipeline *list);
+
 #endif
