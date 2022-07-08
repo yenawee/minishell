f8aac7d6f1bb36b86487bc752b3edd1a57ea55ef
jhj9109 <jhj91_09@naver.com>
2022-07-08 20:26:22 +0900

    [MODIFY] add exit_command_perror_msg

--- utils/exit_msg.c
+++ utils/exit_msg.c
@@ -5,3 +5,16 @@ void	exit_msg(int exit_status, int fd, char *msg)
 	ft_putstr_fd(fd, msg);
 	exit(exit_status);
 }
+
+void	exit_command_perror_msg(int exit_status, char *cmd, char *msg)
+{
+	const char	*project_name = "minishell";
+
+	ft_putstr_fd(project_name, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	exit(exit_status);
+}
