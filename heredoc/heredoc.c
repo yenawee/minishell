77d2a67619e528bce26b7c730af668e7d3392888
jhj9109 <jhj91_09@naver.com>
2022-07-10 17:44:47 +0900

    [MODIFY] heredoc ctrl c & d 반응 구현

--- heredoc/heredoc.c
+++ heredoc/heredoc.c
@@ -6,27 +6,36 @@
 /*   By: hyeonjan <hyeonjan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 16:37:12 by hyeonjan          #+#    #+#             */
-/*   Updated: 2022/07/10 16:39:18 by hyeonjan         ###   ########.fr       */
+/*   Updated: 2022/07/10 17:39:26 by hyeonjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+static void	_end_prompt(int signal)
+{
+	(void)signal;
+	rl_replace_line("", 0);
+	write(1, "\n", 1);
+	rl_on_new_line();
+	exit(EXIT_FAILURE);
+}
+
 void	_set_heredoc(char **file_name, int *fd, int i)
 {
-	char				buf[11];
+	char				buf[12];
 	const char			*alnum = "0123456789abcdef";
 
-	ft_strlcpy(buf, "./heredoc", 11);
-	buf[9] = alnum[i];
-	buf[10] = '\0';
+	ft_strlcpy(buf, "./heredoc_", 12);
+	buf[10] = alnum[i];
+	buf[11] = '\0';
 	*file_name = ft_alert_strdup(buf);
 	*fd = open(*file_name, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 	if (*fd < 0)
 		exit_msg(EXIT_FAILURE, STDERR_FILENO, "fail open()\n");
 }
 
-int	_read_from_heredoc(char **eof, t_list *env_list, int i)
+void	_read(char **eof, t_list *env_list, int i)
 {
 	char	*file_name;
 	int		fd;
@@ -37,20 +46,17 @@ int	_read_from_heredoc(char **eof, t_list *env_list, int i)
 	while (42)
 	{
 		safe_free((void **)&input);
-		input = readline("heredoc 🐚 > ");
-		if (input == NULL || ft_strcmp(input, *eof) == 0)//어느때 NULL이 생기는지 확인해야함! => 시그널로 추측
-		{
+		input = readline("heredoc 🐚> ");
+		if (input == NULL)
+			write(1, "\n", 1);
+		if (input == NULL || ft_strcmp(input, *eof) == 0)
 			break ;
-		}
 		expand_in_heredoc(input, fd, env_list);
 	}
 	close(fd);
 	safe_free(eof);
 	*eof = file_name;
-	if (input == NULL)//어느때 NULL이 생기는지 확인해야함! => 시그널로 추측
-		return (FAIL);
 	safe_free((void **)&input);
-	return (SUCCESS);
 }
 
 int	_is_over_max_heredoc(t_token *tokens)
@@ -69,25 +75,31 @@ int	_is_over_max_heredoc(t_token *tokens)
 	return (SUCCESS);
 }
 
-//cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof && cat << eof
 int	handle_heredoc(t_token *tokens, t_list *env_list)
 {
-	t_token	*cur;
 	int		i;
+	pid_t	pid;
 
 	if (!_is_over_max_heredoc(tokens))
 	{
 		ft_putstr_fd(STDERR_FILENO, "🐚: maximum here-document count exceeded\n");
 		return (FAIL);
 	}
-	i = 0;
-	cur = tokens;
-	while (cur)
+	signal(SIGINT, SIG_IGN);
+	pid = fork();
+	if (pid == -1)
+		exit_msg(10, STDERR_FILENO, "fail fork()\n");
+	else if (pid == 0)
 	{
-		if (cur->type == T_HEREDOC && \
-			!_read_from_heredoc(&cur->next->str, env_list, i++))
-			return (FAIL);
-		cur = cur->next;
+		signal(SIGINT, _end_prompt);
+		i = 0;
+		while (tokens)
+		{
+			if (tokens->type == T_HEREDOC)
+				_read(&tokens->next->str, env_list, i++);
+			tokens = tokens->next;
+		}
+		exit(EXIT_SUCCESS);
 	}
-	return (SUCCESS);
+	return (wait_child(pid) == 0);
 }
