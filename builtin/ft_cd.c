8bc1e67e5d1f8f7d85c5c3629fe337693d6d6966
jhj9109 <jhj91_09@naver.com>
2022-07-14 10:57:02 +0900

    [MODIFU] cd - 성공후 cwd 출력

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: hyeonjan <hyeonjan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 18:26:57 by yenawee           #+#    #+#             */
-/*   Updated: 2022/07/14 10:40:00 by hyeonjan         ###   ########.fr       */
+/*   Updated: 2022/07/14 10:56:01 by hyeonjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,14 @@ static void	_alert_getcwd(char **pwd)
 		exit_msg(EXIT_FAILURE, STDERR_FILENO, "fail getcwd()\n");
 }
 
-static int	_handle_prev_path(char *str, char **target, t_list *env_list)
+static int	_handle_prev_path(char *str, char **target, t_list *env_list, int *print_pwd)
 {
+	*target = NULL;
+	*print_pwd = 0;
 	if (str == NULL || ft_strcmp(str, "-") != 0)
 		return (SUCCESS);
 	*target = get_env_value(env_list, "OLDPWD");
+	*print_pwd = 1;
 	if (*target == NULL)
 	{
 		ft_putstr_fd(STDERR_FILENO, "minishell: cd: OLDPWD not set\n");
@@ -65,9 +68,9 @@ int	ft_cd(t_list *env_list, char **argv)
 	char	*oldpwd;
 	char	*target;
 	char	*cwd;
+	int		print_pwd;
 
-	target = NULL;
-	if (!_handle_prev_path(argv[1], &target, env_list) || \
+	if (!_handle_prev_path(argv[1], &target, env_list, &print_pwd) || \
 		!_handle_home(argv[1], &target, env_list))
 		return (EXIT_FAILURE);
 	_alert_getcwd(&oldpwd);
@@ -79,6 +82,8 @@ int	ft_cd(t_list *env_list, char **argv)
 		return (_return(EXIT_FAILURE, target, oldpwd, NULL));
 	}
 	_alert_getcwd(&cwd);
+	if (print_pwd)
+		printf("%s\n", cwd);
 	ft_export_one(&env_list, "PWD", cwd, 0);
 	ft_export_one(&env_list, "OLDPWD", oldpwd, 0);
 	return (_return(EXIT_SUCCESS, target, oldpwd, cwd));
