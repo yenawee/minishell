654a7c118e994e218e64a252a85a997dcbb55604
jhj9109 <jhj91_09@naver.com>
2022-07-14 20:27:13 +0900

    [HOTFIX] get_env_value, value NULL 대응

--- env/get_env_value.c
+++ env/get_env_value.c
@@ -6,7 +6,7 @@
 /*   By: hyeonjan <hyeonjan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 19:22:00 by yenawee           #+#    #+#             */
-/*   Updated: 2022/07/14 09:40:24 by hyeonjan         ###   ########.fr       */
+/*   Updated: 2022/07/14 20:26:13 by hyeonjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,11 @@ char	*get_env_value(t_list *env_list, char *key)
 	while (curr)
 	{
 		if (!ft_strcmp(((t_env *)curr->content)->key, key))
-			return (ft_alert_strdup(((t_env *)curr->content)->value));
+		{
+			if (((t_env *)curr->content)->value)
+				return (ft_alert_strdup(((t_env *)curr->content)->value));
+			return (NULL);
+		}
 		curr = curr->next;
 	}
 	return (NULL);
